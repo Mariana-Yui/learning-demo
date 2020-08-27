8ee447d18a7bd9ebefeb56583e41e35fc957f48d
mariana-yui <HealMSlin@gmail.com>
2020-08-27 23:50:24 +0800

    feat: commit

--- ImageCropper/index.js
+++ ImageCropper/index.js
@@ -5,23 +5,45 @@ class ImageCropper {
         this.targetRef = document.querySelector('.target');
         this.canvasRef = document.querySelector('canvas');
         this.dataUrl = '';
+        this.start = {
+            x: 0,
+            y: 0
+        };
+        this.end = {
+            x: 0,
+            y: 0
+        };
     }
     // input上传本地文件触发操作
     handleChange() {
         const file = this.file.files[0];
         const fileReader = new FileReader();
         fileReader.onload = (event) => {
-            this.dataUrl = event.target.result;
-            this.sourceRef.src = this.dataUrl;
+            const dataUrl = event.target.result;
+            this.sourceRef.src = dataUrl;
             this.sourceRef.onload = () => this.drawImage();
         };
         fileReader.readAsDataURL(file);
     }
+    // 鼠标事件
+
     // 裁剪图片，生成裁剪后图片
     handleCrop() {
-        const imageData = this.canvasRef.i;
+        const ctx = this.canvasRef.getContext('2d');
+        const imageData = ctx.getImageData(100, 100, 100, 100);
+        const avatar = document.createElement('canvas');
+        avatar.width = 100;
+        avatar.height = 100;
+        const avatarCtx = avatar.getContext('2d');
+        avatarCtx.putImageData(imageData);
+        this.dataUrl = avatar.toDataURL();
+        this.targetRef.src = this.dataUrl;
     }
     bindEvents() {
         this.file.addEventListener('change', this.handleChange.bind(this));
     }
 }
+
+const imageCropper = new ImageCropper();
+
+imageCropper.bindEvents();
